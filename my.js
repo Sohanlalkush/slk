c751630e51fb0bfed9e7a07da90d24e13e937b64
Sohanlalkush <100574470+Sohanlalkush@users.noreply.github.com>
2022-03-05 11:46:27 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,4 +1,4 @@
-document.getElementById("subi").src = 'demo.jpg'; 
+document.getElementById("subi").src = 'https://drive.google.com/uc?export=view&id=1uqdikgSjrfJOIX2Vj1Gq29l3A92MnSTM'; 
 
 let subn = 'Advanced Drug Delivery System  (Nirali Prakashan).pdf';
 document.getElementById("subn").innerHTML = subn;
