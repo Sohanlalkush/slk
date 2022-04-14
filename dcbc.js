56de55fb51bfab555fcc23e611fec8f1769ab0fe
Sohanlalkush <100574470+Sohanlalkush@users.noreply.github.com>
2022-04-15 01:02:00 +0530

    Create dcbc.js

---
+++ dcbc.js
@@ -0,0 +1,304 @@
+let n101a = ' PharmaLite.in Human Anatomy and Physiology I - Thakur Publication.pdf ';
+ let l101a =  '1suLsUTWy6V2klwkSIv0NQYRWO6VpaNfk' ;
+
+let n101c = ' PharmaLite.in Human Anatomy and Physiology - I (Nirali Prakashan).pdf ';
+ let l101c =  '1uSHXucTsrvcrs6o4ec-qu7-sA7Lh8LEt' ;
+
+let n102b = ' PharmaLite.in Pharmaceutical Analysis (PV).pdf ';
+ let l102b =  '13hiLxqazfmJUx2g8I8TUfQ6v0Yy1rOCL' ;
+
+let n102c = ' PharmaLite.in Pharmaceutical Analysis (Nirali Prakashan).pdf ';
+ let l102c =  '1_MO1M6Z381c_e-JM1q9D9HpskyhsfwR1' ;
+
+let n103c = ' PharmaLite.in Pharmaceutics - I (Nirali Prakashan).pdf ';
+ let l103c =  '16RppX93B_DwH-3ct6Qos45K0ZWVhcom9' ;
+
+let n104c = ' PharmaLite.in Pharmaceutical Inorganic Chemistry (Nirali Prakashan).pdf ';
+ let l104c =  '1bOw4121gj_HmzyUI1VDig8S3aq92O70R' ;
+
+let n105c = ' PharmaLite.in Remidial Biology (Nirali Prakashan).pdf ';
+ let l105c =  '18Zqx8DxgE1b299QjnsqqW9Tp5gUN--Cv' ;
+
+let n106c = ' PharmaLite.in Remedial Mathematics (Nirali Prakashan).pdf ';
+ let l106c =  '1_tvYU_D97ZUjgXRuayYi2otn8qmN13Kl' ;
+
+let n201a = ' PharmaLite.in Human Anatomy and Physiology - II (Thakur Publication).pdf ';
+ let l201a =  '1G3LmfpWAY1dy6KNEN9HMjMpmNNk7nlt5' ;
+
+let n201c = ' PharmaLite.in Human Anatomy and Physiology - II (Nirali Prakashan).pdf ';
+ let l201c =  '1WPKYHwGKrb0_z1Vq73KOI1nbP0CNG6n5' ;
+
+let n202a = ' PharmaLite.in Pharmaceutical Organic Chemistry - I (Thakur Publication).pdf ';
+ let l202a =  '1plP9F41EZca0Y2IguXAOe3xwr-Qa-INx' ;
+
+let n202b = ' PharmaLite.in Pharmaceutical Organic Chemistry - I (PV Publication).pdf ';
+ let l202b =  '1dRBC0yYXRjH0Z7mtLCIlE42LSzepB81E' ;
+
+let n202c = ' PharmaLite.in Pharmaceutical Organic Chemistry - I (Nirali Prakashan).pdf ';
+ let l202c =  '1a-RJUSPxfQxu4ytbfS_KdLgfJ16Y-wVM' ;
+
+let n203a = ' PharmaLite.in Biochemistry (Thakur Publication).pdf ';
+ let l203a =  '1TtWS_HjgOZQ3oIq_mxL0f_b82HHN2804' ;
+
+let n203c = ' PharmaLite.in Biochemistry (Nirali Prakashan).pdf ';
+ let l203c =  '132wNGjs2CoEH7nee1JEPamXwQamBinsB' ;
+
+let n204b = ' PharmaLite.in Pathophysiology (PV Publication).pdf ';
+ let l204b =  '1LYaMIDt4EHtUuwA-N_6CNDjJmhI4gpmh' ;
+
+let n204c = ' PharmaLite.in Patholophysiology (Nirali Prakashan).pdf ';
+ let l204c =  '1hv_rsjEh2Y4ycUcl-84VelyYQStNv9X4' ;
+
+let n205a = ' PharmaLite.in Computer Applications In Pharmacy - Thakur Publicaation.pdf ';
+ let l205a =  '1eVraP2NF8Eey-JoxeTaHy2fTSB6Dkg6j' ;
+
+let n205b = ' PharmaLite.in Computer Applications In Pharmacy (PV Publication).pdf ';
+ let l205b =  '1UXh2xqQqLe2AKyIhGz9Na__wAFpK2rCl' ;
+
+let n206a = ' PharmaLite.in Environmental Sciences (Thakur Publication).pdf ';
+ let l206a =  '15Nyu4rt7XMdVprYEt6ZVcZN66L-4Khbq' ;
+
+let n301a = ' PharmaLite.in Pharmaceutical Organic Chemistry - II (Thakur Publication).pdf ';
+ let l301a =  '1Z0IUUhuQ4juLDISXVSgmsoHbtcX7pSUA' ;
+
+let n301b = ' PharmaLite.in Pharmaceutical Organic Chemistry - II (PV Publication).pdf ';
+ let l301b =  '1EGwRhWdwCQG0KAtugNgYTos3gG_zMJRw' ;
+
+let n301c = ' PharmaLite.in Pharmaceutical Organic Chemistry - II (Nirali Prakashan).pdf ';
+ let l301c =  '1Q4OiRhAqfwjmg9WeG-Ep77Geh7VvniOG' ;
+
+let n302a = ' PharmaLite.in Physical Pharmaceutics - I (Thakur Publication).pdf ';
+ let l302a =  '1hF3-gjTyTJsqa2M8z6rLJAwjRcP8zIqK' ;
+
+let n302b = ' PharmaLite.in Physical Pharmaceutics -I (PV Publication).pdf ';
+ let l302b =  '1CStC5FQq9Bnf1R30478LK5jWAhIZOo10' ;
+
+let n302c = ' PharmaLite.in Physical Pharmacutics - I (Nirali Prakashan).pdf ';
+ let l302c =  '1Zc8w3RNG--SWXRAI8-B0D4TZZ2FG2xmD' ;
+
+let n303a = ' PharmaLite.in Pharmaceutical Microbiology (Thakur Publication).pdf ';
+ let l303a =  '1fGOkC5_YiFKazkEwpj8gIO2g58Xgenit' ;
+
+let n303b = ' PharmaLite.in Pharmaceutical Microbiology (PV Publication).pdf ';
+ let l303b =  '1qXD7sLMRgzMHNX4S42J_S1sNNLKNElB1' ;
+
+let n303c = ' PharmaLite.in Pharmaceutical Microbiology (Nirali Prakashan).pdf ';
+ let l303c =  '1H9n80AXNtvGj5FrMUr3CmAKrT1EyNVMS' ;
+
+let n304a = ' PharmaLite.in Pharmaceutical Engineering - Thakur Publication.pdf ';
+ let l304a =  '1GHijry7C2E2poel1LnT7kVcQSCNpBtlr' ;
+
+let n304b = ' PharmaLite.in Pharmaceutical Engineering (PV Publication).pdf ';
+ let l304b =  '1EgaQfTolhSVD4ZSxJyMy4RMaHu7PD2M9' ;
+
+let n304c = ' PharmaLite.in Pharmaceutical Engineering- Nirali Publication.pdf ';
+ let l304c =  '1Q6YeKXb5qi41JQQtXTqQ9ApALZzqgrLN' ;
+
+let n401b = ' PharmaLite.in Pharmaceutical Organic Chemistry - III (PV Publication).pdf ';
+ let l401b =  '1xp_TbA9QFax_9gnPp0Sy6oVrR5KAUIE2' ;
+
+let n401c = ' PharmaLite.in Pharmaceutical Organic Chemistry - III (Nirali Prakashan).pdf ';
+ let l401c =  '112O690YmFYmYIF_KFrNVRVDlpLj0UOAK' ;
+
+let n402b = ' PharmaLite.in Medicinal Chemistry - I (PV Publication).pdf ';
+ let l402b =  '1jgY6lSkYvt3uvTnZl7N4OYhByQASlRmM' ;
+
+let n402c = ' PharmaLite.in Medicinal Chemistry - I (Nirali Prakashan).pdf ';
+ let l402c =  '1ovp1bBVSB0Smas5GUVUjppemjP7El5kg' ;
+
+let n403b = ' PharmaLite.in Physical Pharmaceutics - II (PV Publication).pdf ';
+ let l403b =  '1pHhL960_YFrur7LBHCZVpL4Po5YOhtg6' ;
+
+let n403c = ' PharmaLite.in Physical Pharmaceutics- II (Nirali Prakashan).pdf ';
+ let l403c =  '15TdVHfjN07kvBkmkfIaFgc_4kmxLF9Jc' ;
+
+let n404a = ' PharmaLite.in Pharmacology-I (Thakur Publication).pdf ';
+ let l404a =  '1h4EW7oUZdOxIUNptz5sv8WmWrG3kYr1o' ;
+
+let n404b = ' PharmaLite.in Pharmacology - I (PV Publication).pdf ';
+ let l404b =  '1zyigd0bhjqo25uq7dWvnuvQurw93QjUq' ;
+
+let n404c = ' PharmaLite.in Pharmacology - I (Nirali Prakashan).pdf ';
+ let l404c =  '1ST7EB1rU8vu-ogVn-YIVaDq8Ist4d0Co' ;
+
+let n405b = ' PharmaLite.in Pharmacognosy and Phytochemistry - I (PV Publication).pdf ';
+ let l405b =  '1jHLa04868i7ri94uRRRpGK4A_MxHCUUc' ;
+
+let n405c = ' PharmaLite.in Pharmacognosy and Phytochemistry - I (Nirali Prakashan).pdf ';
+ let l405c =  '1jlgUOUndsFXM3wx8e8mnL1P3DniP2ykx' ;
+
+let n501a = ' PharmaLite.in Medicinal Chemistry II - Thakur Publication.pdf ';
+ let l501a =  '1ROIIxaAj7YHZSNbYkNOwhckmTXE1p_m8' ;
+
+let n501b = ' PharmaLite.in Medicinal Chemistry - II (PV Publication).pdf ';
+ let l501b =  '1jN-D1gSElYnTWeWAdVftDxG_r9ZZJXCs' ;
+
+let n502a = ' PharmaLite.in Industrial Pharmacy-I (Thakur Publication).pdf ';
+ let l502a =  '1BsMD81UMxnTV8UQsyeNfCfIzOvtj7Gnk' ;
+
+let n502b = ' PharmaLite.in Industrial Pharmacy - I (PV Publication).pdf ';
+ let l502b =  '1yubnxU3wrU5yH0WcpXzd1BYQ2YPLc911' ;
+
+let n502c = ' PharmaLite.in Industrial Pharmacy - I (Nirali Prakashan).pdf ';
+ let l502c =  '1L_wGB5FRG0eDoSbcyySh2kpON6g8Pf0Z' ;
+
+let n503a = ' PharmaLite.in Pharmacology - II (Thakur Publication).pdf ';
+ let l503a =  '1q0nR_txZvlm-zmBPSSpic_EqHfEu7dYz' ;
+
+let n503b = ' PharmaLite.in Pharmacology - II (PV Publication).pdf ';
+ let l503b =  '1n2tx2KLvQYxjfGg_V6QIUbYSz0cULanb' ;
+
+let n503c = ' PharmaLite.in Pharmacology - II (Nirali Prakashan).pdf ';
+ let l503c =  '1KXSaDyO9QIWqRvWF0hSbqbIJI-geZB5O' ;
+
+let n504a = ' PharmaLite.in Pharmacognosy and Phytochemistry -II (Thakur Publication).pdf ';
+ let l504a =  '1pkQnNLo3QX-1zG0qCU63tgf379Th5qFW' ;
+
+let n504b = ' PharmaLite.in Pharmacognosy and phytochemistry- II (PV Publication).pdf ';
+ let l504b =  '1sXX2yIsWw8u-lBe8rQ6-7Cvue1OKIMfC' ;
+
+let n504c = ' PharmaLite.in Pharmacognosy and Phytochemistry - II (Nirali Prakashan).pdf ';
+ let l504c =  '1c_6ohUGern7elyBTnkkvUSV4qsK1tK-s' ;
+
+let n505a = ' PharmaLite.in Pharmaceutical Jurisprudence - Thakur Publication.pdf ';
+ let l505a =  '1WHcz5efhhpPzz95Kch6YDX6ROV8bT3c9' ;
+
+let n505b = ' PharmaLite.in Pharmaceutical Jurisprudence (PV Publication).pdf ';
+ let l505b =  '1Qyih-FCCPW2fykjzxjpY89kAfj4I1ofS' ;
+
+let n505c = ' PharmaLite.in Pharmaceutical Jurisprudence (Nirali Prakashan).pdf ';
+ let l505c =  '1g9lC4YWUbH7GKdhFIXj5jdnG-mEWtHRa' ;
+
+let n601a = ' PharmaLite.in Medicinal Chemistry - III (Thakur Publication).pdf ';
+ let l601a =  '1ieDZ08IM7Hws9TbRLwUVBVN-3BqjuhR4' ;
+
+let n601b = ' PharmaLite.in Medicinal Chemistry - III (PV Publication).pdf ';
+ let l601b =  '1CCsFWk-I5mF7OTnrlVugacRNRJxj6ELn' ;
+
+let n601c = ' PharmaLite.in Medicinal Chemistry - III -K.G. BOTHERA (Nirali Prakashan).pdf ';
+ let l601c =  '19VdHEkI8RqAV1hWmYPKA5aw0A_LA2SA5' ;
+
+let n602b = ' PharmaLite.in Pharmacology - III (PV Publication).pdf ';
+ let l602b =  '1aW0RjO08YVxn94FWxC5mqV5W_Sn201nu' ;
+
+let n602c = ' PharmaLite.in Pharmacology - III (Nirali Prakashan).pdf ';
+ let l602c =  '1wE41JyMF9fb-2RA8Tnvml7Iw19_M4tXV' ;
+
+let n603a = ' PharmaLite.in Herbal Drug Technology (Thakur Publication).pdf ';
+ let l603a =  '1p6BzK-78suZGVjhDJAWToks5eoQ83TwE' ;
+
+let n603b = ' PharmaLite.in Herbal Drug Technology (PV Publication).pdf ';
+ let l603b =  '1gptCdSR1mxvSkVqFswFT2kPuoHfLt9Ad' ;
+
+let n603c = ' PharmaLite.in Herbal Drug Technology (Nirali Prakashan).pdf ';
+ let l603c =  '1txsOZUAQ-2vK16bCTVdwfeRGIKF6bXiD' ;
+
+let n604a = ' PharmaLite.in Biopharmaceutics & Pharmacokinetics (Thakur).pdf ';
+ let l604a =  '1HKfJr0neyfTro9UrquLTBiRNmnBRkV4p' ;
+
+let n604b = ' PharmaLite.in Biopharmaceutics and Pharmacokinetics (PV Publication).pdf ';
+ let l604b =  '1FB56CjK8Wdj6erU4p57Wt_CF8bsMqvpq' ;
+
+let n604c = ' PharmaLite.in Pharmaceutical Biotechnology (Nirali Prakashan).pdf ';
+ let l604c =  '1ZX4Vz2sTC_czG6G8U69O2VWWf6LxkiU_' ;
+
+let n605a = ' PharmaLite.in Pharmaceutical Biotechnology (Thakur).pdf ';
+ let l605a =  '1oOJv7eqi1n55Kqap1DtpdP5Iyp1wlwKq' ;
+
+let n605b = ' PharmaLite.in Pharmaceutical Biotechnology (PV Publication).pdf ';
+ let l605b =  '1i76uNKZmIFNuQjESTVxfSQF_xtn9iE_W' ;
+
+let n606b = ' PharmaLite.in Pharmaceutical Quality Assurance (PV Publication).pdf ';
+ let l606b =  '1-8csbvw3LGrMHSsquvwYNW_BkoPH9Hh2' ;
+
+let n606c = ' PharmaLite.in Pharmaceautical Quality Assurance (Nirali Prakashan).pdf ';
+ let l606c =  '1sH-YTWMHHIKY4mEpeFtTRsQSvcwgdJlD' ;
+
+let n701a = ' PharmaLite.in Instrumental Method of Analysis - Thakur Publication.pdf ';
+ let l701a =  '1QqACdmgHNevhf99jqwHaw0RZ61IGAJ3t' ;
+
+let n701b = ' PharmaLite.in Instrumental Method of Analysis (Nirali Prakashan).pdf ';
+ let l701b =  '1iAehAU09n6VcQ-yc8QYM8rxH-O5eeZcF' ;
+
+let n701c = ' PharmaLite.in Instrumental Method of Analysis (PV Publication).pdf ';
+ let l701c =  '1KQHtoLtrsGnNt2vbKbT4U3thDc-0s9j2' ;
+
+let n702a = ' PharmaLite.in Industrial Pharmacy - II (Thakur Publication).pdf ';
+ let l702a =  '10UKX_jg3HtL-V-6BpiI-uqp-tyhODK28' ;
+
+let n702b = ' PharmaLite.in Industrial Pharmacy -II (PV Publication).pdf ';
+ let l702b =  '1aR7fKenRG3pW4Hh89Igxr35JCcnB-wBb' ;
+
+let n702c = ' PharmaLite.in Industrial Pharmacy - II (Nirali Prakashan).pdf ';
+ let l702c =  '156lMJF5NQU3BwnNf3PvCMKksFmZ5bGA0' ;
+
+let n703b = ' PharmaLite.in Pharmacy Practice (PV Publication).pdf ';
+ let l703b =  '15u0l-mN5I4T6FqPzTrTw7OOOsiaMAAC-' ;
+
+let n703c = ' PharmaLite.in Pharmacy Practice (Nirali Prakashan).pdf ';
+ let l703c =  '1F8GeGDvmOL9JQmX9Ovl-4X2ElIh12EDI' ;
+
+let n704a = ' PharmaLite.in Novel Drug Delivery Systems - Thakur Publication.pdf ';
+ let l704a =  '1USqbCBFqm4gNzfJcs9YyeByL4SYZ1EaJ' ;
+
+let n704b = ' PharmaLite.in Novel Drug Delivery Systems (PV Publication).pdf ';
+ let l704b =  '1QLCPjCukp2mW0Cp643vLXzLeiuHcYNzY' ;
+
+let n704c = ' PharmaLite.in Novel Drug Delivery Systems (Nirali Prakashan).pdf ';
+ let l704c =  '1eanbY3PKZPZ5Gn7CPCVYF_LxwotIGITj' ;
+
+let n801a = ' PharmaLite.in Biostatistics and Research Methodology (Thakur Publication).pdf ';
+ let l801a =  '1QX7UORbwzOaKB8mGkDcrVxfdh2f5Ig1H' ;
+
+let n801b = ' PharmaLite.in Biostatistics and Research Methodology (PV Publication).pdf ';
+ let l801b =  '1MDQVSyxcBZjhe2AtI994PDEItr1Ip2wI' ;
+
+let n801c = ' PharmaLite.in Biostatistics and Research Methodology (Nirali Prakashan).pdf ';
+ let l801c =  '1z5cGuGIoQ7mxkUiZlTLnWKQ5R8b2k5m6' ;
+
+let n802a = ' PharmaLite.in Social And Preventive Pharmacy (Thakur Publication).pdf ';
+ let l802a =  '143o3aBJSh7fIYRnsJoJU-AWPRQ-Ha8zS' ;
+
+let n802b = ' PharmaLite.in Social and Preventive Pharmacy (PV Publication).pdf ';
+ let l802b =  '1UZJpN6UGPTkVDPIjCyN3Q6kY4-8wm_dY' ;
+
+let n802c = ' PharmaLite.in Social and Preventive Pharmacy Nirali.pdf ';
+ let l802c =  '18YA5pO4u3F_YEKCJxfoki_SB8bEi4WhT' ;
+
+let n803c = ' PharmaLite.in Pharma Marketing Management (Nirali Prakashan).pdf ';
+ let l803c =  '1dV_VczaPlt5UWB7seSF-z4LlWDiEKL9a' ;
+
+let n804c = ' PharmaLite.in Pharmaceutical Regulatory Science (Nirali Prakashan).pdf ';
+ let l804c =  '1XGCQE_DoWrWeya5K648ubnvx0X7JTeYr' ;
+
+let n805c = ' PharmaLite.in Pharmacovigilance (Nirali Prakashan).pdf ';
+ let l805c =  '1l9BlKLfZwFFTiKuxf_uf1MwQUwVbM2tZ' ;
+
+let n806c = ' PharmaLite.in Quality_Control_and_Standardization_of_HerbalS_Nirali_Prakashan.pdf ';
+ let l806c =  '1hNlHqLbmULhO8T9ZoZBetX3izaN7_AlS' ;
+
+let n807b = ' PharmaLite.in Computer Aided Drug Design (PV Publication).pdf ';
+ let l807b =  '1FwhqGox-PCLFvKT5pq-jSwTfAXHuJt_o' ;
+
+let n809b = ' PharmaLite.in Cosmetic Science (PV Publication).pdf ';
+ let l809b =  '1HHNH6u4L2-2t-a8Cw9Q66vE3dqfFyROH' ;
+
+let n809c = ' PharmaLite.in Cosmetic Science (Nirali Prakashan).pdf ';
+ let l809c =  '1klFF_W75YnB8R_V3UCO9MWvk9-bAlL1q' ;
+
+let n811b = ' PharmaLite.in Advanced Instrumentation Techniques (PV Publication).pdf ';
+ let l811b =  '1PfZ62Y8CPRiG6aWAUuUh66XZni_XNgLn' ;
+
+let n811c = ' PharmaLite.in Advanced Instrumentation Techniques (Nirali Prakashan).pdf ';
+ let l811c =  '1sD761SIEX6cFzgiTWvdGkCFCsElYcuzZ' ;
+
+let n813c = ' PharmaLite.in Pharmaceutical Product Development Nirali Sem -8.pdf ';
+ let l813c =  '1-cTPB8l7jpYqfydRnm4si7kaCNhc_flB' ;
+
+let n818c = ' PharmaLite.in Advanced Drug Delivery System (Nirali Prakashan).pdf ';
+ let l818c =  '15wKAO4zZVLO3Ev9hQ4c75QQETMiCRj8y' ;
+let gdown = '../../p/gddown.html?fl=';
+let buy = '../../p/buy.html?pn=';
+let dl2 = '&fn=';
+let dpview = 'https://drive.google.com/file/d/' ;
+
